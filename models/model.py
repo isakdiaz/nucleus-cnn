8dfaa3931277ebeecdff8baa0d03bee0db6f8738
Isak <isakdiaz@gmail.com>
2018-02-10 14:39:05 -0500

    Removed unnecessary import statements

--- models/model.py
+++ models/model.py
@@ -4,6 +4,7 @@ from keras.layers.core import Dropout, Lambda
 from keras.layers.convolutional import Conv2D, Conv2DTranspose
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.merge import concatenate
+from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import backend as K
 
@@ -21,22 +22,26 @@ class UNET(Model):
         c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
         c1 = Dropout(0.1) (c1)
         c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
+        c1 = BatchNormalization(name='block1_norm')(c1)
         p1 = MaxPooling2D((2, 2)) (c1)
 
         c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
         c2 = Dropout(0.1) (c2)
         c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
+        c2 = BatchNormalization(name='block2_norm')(c2)
         p2 = MaxPooling2D((2, 2)) (c2)
 
         c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
         c3 = Dropout(0.2) (c3)
         c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
+        c3 = BatchNormalization(name='block3_norm')(c3)
         p3 = MaxPooling2D((2, 2)) (c3)
 
         c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
         c4 = Dropout(0.2) (c4)
         c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
-        p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+        c4 = BatchNormalization(name='block4_norm')(c4)
+        p4 = MaxPooling2D(pool_size=(2, 2))(c4)
 
         c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
         c5 = Dropout(0.3) (c5)
@@ -47,18 +52,21 @@ class UNET(Model):
         c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
         c6 = Dropout(0.2) (c6)
         c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
+        c6 = BatchNormalization(name='block6_norm')(c6)
 
         u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
         u7 = concatenate([u7, c3])
         c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
         c7 = Dropout(0.2) (c7)
         c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
+        c7 = BatchNormalization(name='block7_norm')(c7)
 
         u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
         u8 = concatenate([u8, c2])
         c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
         c8 = Dropout(0.1) (c8)
         c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
+        c8 = BatchNormalization(name='block8_norm')(c8)
 
         u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
         u9 = concatenate([u9, c1], axis=3)

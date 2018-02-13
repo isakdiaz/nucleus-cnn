70fa8bcd2096f9cf3e004c126cbea45d5086966d
isakdiaz <isak@gatech.edu>
2018-02-12 22:09:00 -0500

    Changed model to a function instead of a class

--- models/model.py
+++ models/model.py
@@ -7,76 +7,88 @@ from keras.layers.merge import concatenate
 from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import backend as K
+from metrics.mean_iou import mean_iou
 
 
 
-class UNET(Model):
 
-    def __init__(self, input_shape):         #InputShape -> (H, W, Channels)
+def UNET(input_shape):         #InputShape -> (H, W, Channels)
 
-        # Build U-Net models
+    # Build U-Net models
 
-        inputs = Input(input_shape)
-        s = Lambda(lambda x: x / 255) (inputs)
+    inputs = Input(input_shape)
 
-        c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
-        c1 = Dropout(0.1) (c1)
-        c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
-        c1 = BatchNormalization(name='block1_norm')(c1)
-        p1 = MaxPooling2D((2, 2)) (c1)
+    s = Lambda(lambda x: x / 255) (inputs)
 
-        c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
-        c2 = Dropout(0.1) (c2)
-        c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
-        c2 = BatchNormalization(name='block2_norm')(c2)
-        p2 = MaxPooling2D((2, 2)) (c2)
+    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
+    c1 = Dropout(0.1) (c1)
+    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
+    c1 = BatchNormalization(name='block1_norm')(c1)
+    p1 = MaxPooling2D((2, 2)) (c1)
 
-        c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
-        c3 = Dropout(0.2) (c3)
-        c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
-        c3 = BatchNormalization(name='block3_norm')(c3)
-        p3 = MaxPooling2D((2, 2)) (c3)
+    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
+    c2 = Dropout(0.1) (c2)
+    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
+    c2 = BatchNormalization(name='block2_norm')(c2)
+    p2 = MaxPooling2D((2, 2)) (c2)
 
-        c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
-        c4 = Dropout(0.2) (c4)
-        c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
-        c4 = BatchNormalization(name='block4_norm')(c4)
-        p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
+    c3 = Dropout(0.2) (c3)
+    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
+    c3 = BatchNormalization(name='block3_norm')(c3)
+    p3 = MaxPooling2D((2, 2)) (c3)
 
-        c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
-        c5 = Dropout(0.3) (c5)
-        c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
+    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
+    c4 = Dropout(0.2) (c4)
+    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
+    c4 = BatchNormalization(name='block4_norm')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
 
-        u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
-        u6 = concatenate([u6, c4])
-        c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
-        c6 = Dropout(0.2) (c6)
-        c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
-        c6 = BatchNormalization(name='block6_norm')(c6)
+    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
+    c5 = Dropout(0.3) (c5)
+    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
 
-        u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
-        u7 = concatenate([u7, c3])
-        c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
-        c7 = Dropout(0.2) (c7)
-        c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
-        c7 = BatchNormalization(name='block7_norm')(c7)
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
+    c6 = Dropout(0.2) (c6)
+    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
+    c6 = BatchNormalization(name='block6_norm')(c6)
 
-        u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
-        u8 = concatenate([u8, c2])
-        c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
-        c8 = Dropout(0.1) (c8)
-        c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
-        c8 = BatchNormalization(name='block8_norm')(c8)
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
+    c7 = Dropout(0.2) (c7)
+    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
+    c7 = BatchNormalization(name='block7_norm')(c7)
 
-        u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
-        u9 = concatenate([u9, c1], axis=3)
-        c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
-        c9 = Dropout(0.1) (c9)
-        c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
+    c8 = Dropout(0.1) (c8)
+    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
+    c8 = BatchNormalization(name='block8_norm')(c8)
 
-        outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
+    c9 = Dropout(0.1) (c9)
+    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
 
-        super(Model, self).__init__(inputs, outputs)
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
 
-    def compile(self, optimizer, **kwargs):
-        super(UNET, self).compile(optimizer, **kwargs)
+    model = Model(inputs = inputs, outputs = outputs, name='UNET')
+
+    return model
+
+if __name__ == "__main__":
+
+    IMG_WIDTH = 128
+    IMG_HEIGHT = 128
+    IMG_CHANNELS = 3
+
+    model = UNET((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+    model.summary()
+    model.save('test.h5')
+    model2 = load_model('test.h5', custom_objects = {'mean_iou' : mean_iou}) #custom metrics must be loaded as custom objects
